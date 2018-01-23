819d3cb34544a348f9d1dab9de268a595b6e76c6
EnderWigginIII <EnderWigginIII@users.noreply.github.com>
2018-01-23 15:10:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-curl https://raw.githubusercontent/EnderWigginIII/test-files/master/etc/skel/.bashrc > /etc/skel/.bashrc
-curl https://raw.githubusercontent/EnderWigginIII/test-files/master/etc/bash.bashrc > /etc/bash.bashrc
-curl https://raw.githubusercontent/EnderWigginIII/test-files/master/etc/DIR_COLORS > /etc/DIR_COLORS
+curl https://raw.githubusercontent.com/EnderWigginIII/test-files/master/etc/skel/.bashrc > /etc/skel/.bashrc
+curl https://raw.githubusercontent.com/EnderWigginIII/test-files/master/etc/bash.bashrc > /etc/bash.bashrc
+curl https://raw.githubusercontent.com/EnderWigginIII/test-files/master/etc/DIR_COLORS > /etc/DIR_COLORS
